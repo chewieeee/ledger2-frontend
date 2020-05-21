2b071f7e39d2fc4d3f1944866e31a5adaccc639f
Bastian <Pohl>
2020-05-21 22:33:49 +0200

    style updates

--- src/components/documents/AccountSummary.vue
+++ src/components/documents/AccountSummary.vue
@@ -4,13 +4,13 @@
             <v-row>
                <v-col
                   :cols="7" 
-                  class="text-left pl-2 py-0" 
+                  class="text-left pl-2 pr-0" 
                >
                   {{ account.title }}
                </v-col>
                <v-col 
                   :cols="5"
-                  class="text-right pr-1 py-0"
+                  class="text-right px-0 pl-0"
                >
                   {{ 
                      new Intl.NumberFormat("de-DE", format).format(account.balance) 
